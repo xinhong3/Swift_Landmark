5b99fde5c328eae6dc9a969c7200cbf1da6f75fe
Ethan H <jujugregoryh@gmail.com>
2024-06-15 22:43:37 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ethan Hong on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
