a50791bb7291ff6137b15134fcd9457f8ea68474
Ethan H <jujugregoryh@gmail.com>
2024-06-15 23:12:37 -0700

    Basic UI, Views
    Tutorial link:
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
    
    What's covered:
    1. Create seprate views (MapView, CircleImage view) in SwiftView UI
    2. Combine them in ContectView

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ethan Hong on 6/15/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
